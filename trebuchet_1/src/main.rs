826663b5e9ecd0917527d590d1a53188358068e4
Matheus Ferreira <mattferreira.dev@protonmail.com>
2023-12-02 01:00:20 -0300

    refactor day 1

--- trebuchet_1/src/main.rs
+++ trebuchet_1/src/main.rs
@@ -11,16 +11,16 @@ fn get_calibrations() -> Vec<String> {
     content.split('\n').map(str::to_string).collect()
 }
 
-fn sort_by_position<'a>(vector: Vec<(&'a str, u16)>) -> Vec<&'a str> {
+fn sort_by_position(vector: Vec<(String, u16)>) -> Vec<String> {
     let mut positions: Vec<_> = vector.iter().map(|(_, pos)| pos).collect();
-    let mut result: Vec<&str> = vec![];
+    let mut result: Vec<String> = vec![];
 
     positions.sort();
 
     for position in positions {
         match vector.iter().find(|(_, pos)| pos == position) {
             Some(i) => {
-                result.push(i.0);
+                result.push(i.0.clone());
             }
             None => {
                 // do nothing
@@ -31,51 +31,65 @@ fn sort_by_position<'a>(vector: Vec<(&'a str, u16)>) -> Vec<&'a str> {
     result
 }
 
-fn extract_digits_from_calibration(calibration: &String) -> Vec<u16> {
+fn extract_common_digits(calibration: &String) -> Vec<(String, u16)> {
     let digits = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let extensive_digits = vec![
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let mut extracted: Vec<(&str, u16)> = vec![];
+    let mut result: Vec<(String, u16)> = vec![];
 
-    for digit in digits.iter() {
+    for digit in digits {
         let matched: Vec<_> = calibration.match_indices(digit).collect();
 
         matched.iter().for_each(|i| {
-            extracted.push((digit, i.0 as u16));
+            result.push((digit.to_string(), i.0 as u16));
         });
-    }
-
-    for (i, digit) in extensive_digits.iter().enumerate() {
-        let matched: Vec<_> = calibration.match_indices(digit).collect();
-
-        matched.iter().for_each(|m| {
-            extracted.push((digits[i], m.0 as u16));
-        });
-    }
-
-    let digits = sort_by_position(extracted);
-    let mut result: Vec<u16> = vec![];
-
-    for digit in digits {
-        match digit.parse::<u16>() {
-            Ok(n) => {
-                result.push(n);
-            }
-            Err(_) => {
-                // do nothing
-            }
-        }
     }
 
     result
 }
 
-fn extract_digits(calibrations: Vec<String>) -> Vec<Vec<u16>> {
+fn extract_extensive_digits(calibration: &String) -> Vec<(String, u16)> {
+    let extensive_digits = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut result: Vec<(String, u16)> = vec![];
+
+    for (i, digit) in extensive_digits.iter().enumerate() {
+        let matched: Vec<_> = calibration.match_indices(digit).collect();
+        let digit = i.to_string();
+
+        matched.iter().for_each(|m| {
+            result.push((digit.clone(), m.0 as u16));
+        });
+    }
+
+    result
+}
+
+fn extract_digits(calibration: &String) -> Vec<u16> {
+    let mut extracted: Vec<(String, u16)> = vec![];
+
+    extracted.extend(extract_common_digits(calibration));
+    extracted.extend(extract_extensive_digits(calibration));
+
+    let digits = sort_by_position(extracted);
+
+    digits
+        .iter()
+        .map(|digit| match digit.parse::<u16>() {
+            Ok(n) => {
+                return n;
+            }
+            Err(_) => {
+                return 0;
+            }
+        })
+        .collect()
+}
+
+fn extract_all_digits(calibrations: Vec<String>) -> Vec<Vec<u16>> {
     let mut result: Vec<Vec<u16>> = vec![];
 
     for calibration in calibrations.iter() {
-        let digits = extract_digits_from_calibration(calibration);
+        let digits = extract_digits(calibration);
 
         if digits.len() == 0 {
             continue;
@@ -100,7 +114,7 @@ fn unify_digits(digits: &Vec<u16>) -> u16 {
 
 fn main() {
     let calibrations = get_calibrations();
-    let digits = extract_digits(calibrations);
+    let digits = extract_all_digits(calibrations);
     let sum = digits.iter().map(unify_digits).sum::<u16>().clone();
 
     println!("sum of calibrations: {sum}")
